3264a26297cbf98c0525c6ffe2d12375cb1463a4
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-02-22 00:31:36 +0200

    second commit
    1. created layout by array with data.
    2. added addEventListener() on container with pictures, got the link for original size pics

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const containerEl = document.querySelector(".gallery");
+const images = createGalleryItems(galleryItems);
+
+containerEl.insertAdjacentHTML("beforeend", images)
+containerEl.addEventListener('click', onGalleryContainerClick)
+
+function createGalleryItems(images){
+    return images.map(({preview, original, description} )=> 
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+     </div>`  
+    ).join("")
+}
+
+function onGalleryContainerClick(event){
+    if(event.target.nodeName !== "IMG"){
+        return
+    }
+    const originalImageLink = event.target.dataset.source;
+    console.log(originalImageLink)
+
+}
