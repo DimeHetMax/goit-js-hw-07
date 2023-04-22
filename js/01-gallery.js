6155bded08c7e3740f01e87d40c345b65a878742
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-04-23 00:27:49 +0300

    fisrt task
    Realization of markup of the gallery, adding event listener, and using library basicLightbox for opening modal window on click with picture.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+// instance.show()
+
+const ulEl = document.querySelector(".gallery")
+const imageMarkup = createImageMarkup(galleryItems)
+ulEl.insertAdjacentHTML('beforeend', imageMarkup)
+ulEl.addEventListener("click", onImageClick)
+
+function createImageMarkup(images){
+    return images.map(({preview,original,description})=>{
+        return ` 
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </li>`
+    }).join("");
+}
+
+
+
+function onImageClick (event){
+    event.preventDefault()
+    if(event.target.nodeName !== "IMG"  && event.target.nodeName !== "A"){
+        return;
+    }
+    const img = event.target.dataset.source;
+    basicLightbox.create(`
+		<img src="${img}">
+	`).show()
+}
+
+
