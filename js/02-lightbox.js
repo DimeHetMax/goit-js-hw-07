082d5a93444834f103ba3d06f2e3aa06781e48ca
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-04-23 23:59:04 +0300

    second task
    1. create markup
    2. adding script of the  SimpleLightbox library.
    3. adding all the elements to ul.gallery.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+// ==========================================================================
+const ulEl = document.querySelector(".gallery");
+const imageMarkup = createImageMarkup(galleryItems)
+ulEl.insertAdjacentHTML('beforeend', imageMarkup)
+
+function createImageMarkup(images){
+    return images.map(({preview,original,description})=>{
+        return ` 
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" 
+                src="${preview}"
+                alt="${description}" 
+                />
+            </a>
+        </li>`
+    }).join("");
+}
+
+let gallery = new SimpleLightbox('.gallery .gallery__link',{captionsData: "alt", captionDelay: 250});
+
+gallery.on('show.simplelightbox', function () {});
